ccef6f02853a96fb48de267c28874bf6ce2311e8
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 15:15:36 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -9,4 +9,4 @@ class Config(Config):
   RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1FoemGATbwi2dBVitzP3DkCj2uq_anNan\ntoken = {"access_token":"ya29.a0AfH6SMC9ZTaAFI9J67nrVc-_P3P8Mh73D7gvGzcCdoSutZH3rJtv8Zd6Ec2ZghUESpPDAaBQzZBk-CQ1NpDeN7pd0Mn4upY8lJJxh4NB8fWoCXyhAwW5JW7JzDWJ4QEdbhBcV7ipI90tRObk49X0c-nzh-IhppNoUVoPfdniCkg","token_type":"Bearer","refresh_token":"1//0cNaN6be_ZskCCgYIARAAGAwSNwF-L9IrltPBtgutEvyY0BfY6-37unC_4U0bQrAlTuSKsf4RcuLUb7O_S08RDYGzX9F9i11ZD1A","expiry":"2020-12-12T13:54:58.6948751+05:30"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
-  INDEX_LINK = "https://spirit.darkkali614.workers.dev"  
+  INDEX_LINK = "https://spirit.darkkali614.workers.dev/DOWNLOAD"  
