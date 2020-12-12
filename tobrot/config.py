eda1bcc8fc7370d84d009602b086d8a449c5d9af
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 14:42:15 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = ""
+  APP_ID = 
+  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+  AUTH_CHANNEL = [-1001266398622]
