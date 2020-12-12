00bf52859078b69163f6006fb1d74d0f8f7c58be
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-12 13:34:37 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,12 +2,12 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1268fill---your----data"
-    APP_ID = 1373938
-    API_HASH = "fill--your--data"
-    OWNER_ID = 12537936
-    AUTH_CHANNEL = [-10082786282972]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+    TG_BOT_TOKEN= "1436730490:AAGOxPmKhXdB0SgKk-o6v91vc-Lf3uv8UwI"
+    APP_ID = 1383845
+    API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
+    OWNER_ID = 1131653685
+    AUTH_CHANNEL = [-1001266398622]
+    DESTINATION_FOLDER = "DOWNLOAD" #Name of your folder read readme(not id of the folder)
+    RCLONE_CONFIG = """type = drive\nscope = drive\nroot_folder_id = 1FoemGATbwi2dBVitzP3DkCj2uq_anNan\ntoken = {"access_token":"ya29.a0AfH6SMC9ZTaAFI9J67nrVc-_P3P8Mh73D7gvGzcCdoSutZH3rJtv8Zd6Ec2ZghUESpPDAaBQzZBk-CQ1NpDeN7pd0Mn4upY8lJJxh4NB8fWoCXyhAwW5JW7JzDWJ4QEdbhBcV7ipI90tRObk49X0c-nzh-IhppNoUVoPfdniCkg","token_type":"Bearer","refresh_token":"1//0cNaN6be_ZskCCgYIARAAGAwSNwF-L9IrltPBtgutEvyY0BfY6-37unC_4U0bQrAlTuSKsf4RcuLUb7O_S08RDYGzX9F9i11ZD1A","expiry":"2020-12-12T13:54:58.6948751+05:30"}"""
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
