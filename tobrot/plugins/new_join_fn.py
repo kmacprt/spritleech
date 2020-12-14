2e9004a2c6141bb31075b8a09ce522a6c298ebbb
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-14 12:05:45 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,15 +38,15 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""👥 join this group forr help-- @cyberwordk\n\n My Owner: <a href="https://t.me/kavinduaj">SPIRIT</a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
     inline_keyboard = []
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
-            text="read this?",
-            url="https://t.me/keralagram/698909"
+            text="ABOUT",
+            url="visi.tk/kavinduaj"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
