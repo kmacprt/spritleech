3dad0bae106ab61b0b71fc800256b4adbb74ad3e
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-14 11:59:07 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -2,6 +2,7 @@ from tobrot.sample_config import Config
 
 class Config(Config):
   TG_BOT_TOKEN = "1436730490:AAGOxPmKhXdB0SgKk-o6v91vc-Lf3uv8UwI"
+  OWNER_ID = "1131653685" 
   APP_ID = 1383845
   API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
   AUTH_CHANNEL = [-1001266398622]
